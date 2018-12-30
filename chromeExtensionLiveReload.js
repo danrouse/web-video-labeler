de03b667cf63e222ec9a6e0b6f1b51743caa581c
dan <dan@rouse.house>
2018-12-30 17:09:37 -0500

    mixed up parens and livereloading chugged cpu!

--- chromeExtensionLiveReload.js
+++ chromeExtensionLiveReload.js
@@ -10,8 +10,11 @@ function reload() {
 
 async function watchChanges(url, lastHash = '') {
   const hash = await fetch(url).then(res => res.text());
-  if (lastHash && hash !== lastHash) reload();
-  setTimeout(() => watchChanges(url, hash, 1000));
+  if (lastHash && hash !== lastHash) {
+    reload();
+  } else {
+    setTimeout(() => watchChanges(url, hash), 100);
+  }
 }
 
 const hashFileURL = chrome.runtime.getURL('.rollup-hash');
