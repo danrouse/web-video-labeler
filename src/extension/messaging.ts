2c87851bd473fc4fa33c5fd309dba5c902b313b2
dan <dan@rouse.house>
2019-01-02 12:25:14 -0500

    remove script injection bubkis

--- src/extension/messaging.ts
+++ src/extension/messaging.ts
@@ -1,17 +1,31 @@
-// These functions are called from the web context to allow calls to unsafe
-// browser runtime APIs. Messages are handled by the background process.
 export type Message = {
   type: 'FETCH_DOWNLOAD_PATHS';
   filenames: string[];
 };
 
-// Message sender gets bound to Document from content script injector
-declare global {
-  interface Document {
-    _sendMessage: <T>(message: Message) => Promise<T>;
+const sendMessage = <T>(message: Message) =>
+  new Promise<T>(resolve =>
+    chrome.runtime.sendMessage(chrome.runtime.id, message, resolve));
+
+export function fetchDownloadPaths(filenames: string[]) {
+  return sendMessage<string[]>({ filenames, type: 'FETCH_DOWNLOAD_PATHS' });
+}
+
+const getDownloadedPath = async (filename: string) =>
+  new Promise(resolve =>
+    chrome.downloads.search(
+      { filenameRegex: `${filename}$`, limit: 1 },
+      ([{ filename }]) => resolve(filename)),
+    );
+
+function messageHandler(message: Message, _: chrome.runtime.MessageSender, respond: (response: any) => void) {
+  switch (message.type) {
+    case 'FETCH_DOWNLOAD_PATHS':
+      Promise.all(message.filenames.map(getDownloadedPath)).then(paths => respond(paths));
+      return true;
   }
 }
 
-export function fetchDownloadPaths(filenames: string[]) {
-  return document._sendMessage<string[]>({ filenames, type: 'FETCH_DOWNLOAD_PATHS' });
+if (chrome.extension.getBackgroundPage && chrome.extension.getBackgroundPage() === window) {
+  chrome.runtime.onMessage.addListener(messageHandler);
 }
