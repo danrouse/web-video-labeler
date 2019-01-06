ad97757fb24f59c8bbe54a22d5d4711d8c04a86d
dan <dan@rouse.house>
2019-01-06 12:51:37 -0500

    add aws-sdk: it is non-cooperative w rollup, so use iife bundle and prepend minified script

--- rollup.config.js
+++ rollup.config.js
@@ -2,32 +2,31 @@ import resolve from 'rollup-plugin-node-resolve';
 import cjs from 'rollup-plugin-commonjs';
 import css from 'rollup-plugin-css-only';
 import replace from 'rollup-plugin-replace';
-import alias from 'rollup-plugin-alias';
 import hash from 'rollup-plugin-bundle-hash';
-import path from 'path';
+import fs from 'fs';
 
 const OUTPUT_CSS_PATH = 'dist/bundle.css';
 
 export default {
+  external: ['aws-sdk'],
   input: 'dist/main.js',
   output: {
     file: 'dist/bundle.js',
-    format: 'cjs',
-    sourcemap: true
+    format: 'iife',
+    sourcemap: true,
+    globals: { 'aws-sdk': 'AWS' },
+    banner: fs.readFileSync(__dirname + '/node_modules/aws-sdk/dist/aws-sdk.min.js', 'utf-8'),
   },
   plugins: [
     replace({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
-    alias({
-      jszip: path.join(__dirname, './node_modules/jszip/dist/jszip.js')
-    }),
-    resolve({ web: true, browser: true }),
+    resolve({ browser: true, preferBuiltins: false }),
     css({ output: OUTPUT_CSS_PATH }),
     cjs({
       namedExports: {
         'node_modules/react/index.js': ['Children', 'Component', 'PropTypes', 'createElement'],
-        'node_modules/react-dom/index.js': ['render', 'createPortal']
+        'node_modules/react-dom/index.js': ['render', 'createPortal'],
       }
     }),
     hash('dist/.rollup-hash', [OUTPUT_CSS_PATH])
