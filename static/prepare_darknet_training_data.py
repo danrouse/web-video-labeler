363dc3f7230722983de193a626289bc710f6b2d4
dan <dan@rouse.house>
2019-01-06 12:54:23 -0500

    darknet data prep script fixes

--- static/prepare_darknet_training_data.py
+++ static/prepare_darknet_training_data.py
@@ -1,5 +1,6 @@
 #!/bin/python
 import os
+import path
 import re
 import math
 import argparse
@@ -23,26 +24,27 @@ data_dir = args.data_dir
 data_files = [f for f in os.listdir(data_dir) if f.endswith('.darknet-unprepared.txt')]
 classes = set()
 for f in data_files:
-    with open(data_dir + '/' + f, 'r') as df:
+    with open(os.path.join(f), 'r') as df:
         for line in df.read().splitlines():
             classes.add(line.split(' ')[0])
 
 for f in data_files:
-    with open(data_dir + '/' + f, 'r') as df:
+    with open(os.path.join(f), 'r') as df:
         contents = df.read()
         for i, c in enumerate(classes):
             contents = re.sub('^' + c + ' ', str(i) + ' ', contents, flags=re.MULTILINE)
-        with open(data_dir + '/' + f.replace('.darknet-unprepared.txt', '.txt'), 'w') as wf:
+        with open(os.path.join(f.replace('.darknet-unprepared.txt', '.txt')), 'w') as wf:
             wf.write(contents)
 
 with open('names.list', 'w') as f:
     f.write('\n'.join(classes))
 
 split_index = math.ceil(len(data_files) * args.train_test_ratio)
+prepared_paths = [os.path.join(f.replace('.darknet-unprepared.txt', '.jpg')) for f in data_files]
 with open('train.list', 'w') as f:
-    f.write('\n'.join(data_files[:split_index]))
+    f.write('\n'.join(prepared_paths[:split_index]))
 with open('test.list', 'w') as f:
-    f.write('\n'.join(data_files[split_index:]))
+    f.write('\n'.join(prepared_paths[split_index:]))
 
 with open('data.cfg', 'w') as f:
     f.write('\n'.join([
@@ -57,6 +59,6 @@ config = urlopen(args.base_cfg).read().decode('utf-8')
 config = re.sub('^batch=\d+$', 'batch=64', config, flags=re.MULTILINE)
 config = re.sub('^subdivisions=\d+$', 'subdivisions=8', config, flags=re.MULTILINE)
 config = re.sub('^classes=\d+$', 'classes=' + str(len(classes)), config, flags=re.MULTILINE)
-config = re.sub('^filters=255$', '`filters=' + str((len(classes) + 5) * 3), config, flags=re.MULTILINE)
+config = re.sub('^filters=255$', 'filters=' + str((len(classes) + 5) * 3), config, flags=re.MULTILINE)
 with open('network.cfg', 'w') as f:
     f.write(config)
