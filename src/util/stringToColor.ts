b9a5ef9bc5987f8bb0aa0f45f4704a3c6bb172c8
dan <dan@rouse.house>
2019-01-06 12:54:03 -0500

    use different seeded random algo for nicer colors

--- src/util/stringToColor.ts
+++ src/util/stringToColor.ts
@@ -1,21 +1,23 @@
-function djb2(str: string) {
-  let hash = 5381;
-  for (let i = 0; i < str.length; i += 1) {
-    hash = ((hash << 5) + hash) + str.charCodeAt(i); /* hash * 33 + c */
+class Random {
+  static iseed = 1;
+  static seed(str: string) {
+    this.iseed = str.split('').reduce((acc, c) => acc + c.charCodeAt(0), 0);
   }
-  return hash;
+  static get next() {
+    const x = Math.sin(this.iseed) * 10000;
+    this.iseed += 1;
+    return x - Math.floor(x);
+  }
 }
 
 const memo: { [key: string]: string } = {};
 
 export default function hashStringToColor(str: string) {
-  // use bounded HSL (instead of original RGB) to generate a more restricted,
-  // more pleasant set of colors - for relatively high-constrast backgrounds
   if (memo[str]) return memo[str];
-  const hash = djb2(str);
-  const h = hash & 0x0000FF;
-  const s = (hash & 0x00FF00) >> 8;
-  const l = (hash & 0xFF0000) >> 16;
+  Random.seed(str);
+  const h = Math.floor(Random.next * 256);
+  const s = Math.floor(Random.next * 256);
+  const l = Math.floor(Random.next * 256);
   const color = `hsl(${h}, ${30 + ((50 * s) / 256)}%, ${60 + ((30 * l) / 256)}%)`;
   memo[str] = color;
   return color;
